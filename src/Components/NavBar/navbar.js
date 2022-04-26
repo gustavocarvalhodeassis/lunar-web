e38678e5bc1c8b3b71babdd49e6fe180b938efc2
Gustavo Carvalho De Assis <44401808+gustavocarvalhodeassis@users.noreply.github.com>
2022-04-26 13:54:48 -0300

    NavBar start code

--- src/Components/NavBar/navbar.js
+++ src/Components/NavBar/navbar.js
@@ -1,10 +1,52 @@
 import Logo from "./logo";
 
+const navLinks = [
+  {
+    name: "Inicío",
+    hrefid: "home",
+    type: 1
+  },
+  {
+    name: "Sobre",
+    hrefid: "about",
+    type: 1
+  },
+  {
+    name: "Avaliações",
+    hrefid: "reports",
+    type: 1
+  },
+  {
+    name: "Paineis",
+    type: 2
+  },
+  {
+    name: "Contate-nos",
+    hrefid: "contact",
+    type: 3
+  }
+]
+
 function NavBar() {
   return (
     <>
       <header>
-          <Logo />
+        <Logo />
+        <nav className="nav">
+          {
+            navLinks(
+              (link) => {
+                <ul className="nav-links">
+                  <li className="link">
+                    <a href={link.hrefid}>
+                      {link.name}
+                    </a>
+                  </li>
+                </ul>
+              }
+            )
+          }
+        </nav>
       </header>
     </>
   );
