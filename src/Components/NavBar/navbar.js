07cf1263a3b282f67b8816cb3b3d2fef7c743edf
Gustavo Carvalho De Assis <44401808+gustavocarvalhodeassis@users.noreply.github.com>
2022-05-04 19:10:19 -0300

    Some Changes in the NavBar

--- src/Components/NavBar/navbar.js
+++ src/Components/NavBar/navbar.js
@@ -3,33 +3,6 @@ import FilledButton from "../Buttons/filledButton";
 
 import './navbarStyle.css'
 
-const navLinks = [
-  {
-    name: "Inicío",
-    hrefid: "home",
-    type: 1
-  },
-  {
-    name: "Sobre",
-    hrefid: "about",
-    type: 1
-  },
-  {
-    name: "Avaliações",
-    hrefid: "reports",
-    type: 1
-  },
-  {
-    name: "Paineis",
-    type: 2
-  },
-  {
-    name: "Contate-nos",
-    hrefid: "contact",
-    type: 3
-  }
-]
-
 function NavBar() {
   return (
     <>
