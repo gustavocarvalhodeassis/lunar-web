946590fead1001ab4cebd922b6bda70d9c43e12d
Gustavo Carvalho De Assis <44401808+gustavocarvalhodeassis@users.noreply.github.com>
2022-05-26 13:52:11 -0300

    commit master

--- src/Components/NavBar/NavBar2.js
+++ src/Components/NavBar/NavBar2.js
@@ -1,50 +1,41 @@
 import React from "react";
-import { NavLink, Router } from "react-router-dom";
-import {Header, NavBarContainer, Ancora, Logo, DropDown, DropDownLinks, Option,Nav} from'./NavBarComponents'
-import './NavBar Data/NavBarData'
+import {
+  Header,
+  NavBarContainer,
+  Ancora,
+  Logo,
+  DropDown,
+  DropDownLinks,
+  Option,
+  Nav,
+} from "./NavBarComponents";
+
+import "./NavBarData/NavBarData";
 
 function NavBar() {
-    return <>
-        <Header>
+  return (
+    <>
+      <Header>
+        <NavBarContainer>
+          <Logo />
 
-            <NavBarContainer>
+          <Nav>
+            {navLinks.map((navLink) => (
+              <>
+                <Ancora>{navLink.name}</Ancora>
+                <DropDown></DropDown>
+                <DropDownLinks>
+                  <Option value="1"></Option>
 
-                <Logo />
-
-                <Nav>
-                    
-                    <Ancora>
-
-                    </Ancora>
-
-                    <Ancora>
-
-                    </Ancora>
-
-                    <Ancora>
-
-                    </Ancora>
-
-                    <DropDown>
-                      
-                            
-                    </DropDown>
-
-                    <DropDownLinks>
-
-                        <Option value="1">Olá</Option>
-
-                        <Option value="2">Como vai</Option>
-
-                    </DropDownLinks>
-
-                </Nav>
-
-            </NavBarContainer>
-
-        </Header>
-
+                  <Option value="2">Como vai</Option>
+                </DropDownLinks>
+              </>
+            ))}
+          </Nav>
+        </NavBarContainer>
+      </Header>
     </>
+  );
 }
 
-export default NavBar
+export default NavBar;
---
+++ src/Components/NavBar/NavBarData/NavBarData.js
@@ -0,0 +1,33 @@
+const navLinks = [
+    {
+        name: 'início',
+        ancora: '#home'
+    },
+    {
+        name: 'sobre',
+        ancora: '#about'
+    },
+    {
+        name: 'avaliações',
+        ancora: '#reports'
+    },
+    {
+        name: 'painel',
+        ancora: '#',
+        linkList: [
+            {
+                name: 'representante',
+                link: '#'
+            },
+            {
+                name: 'contador',
+                link: '#'
+            },
+        ]
+        
+    },
+    {
+        ancora: '#takePart'
+    }
+]
+
